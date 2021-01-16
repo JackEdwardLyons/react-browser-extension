839616de4535d7a573cdf363f9dcda8f0210c2d3
Jack Lyons <jack.edward.lyons@gmail.com>
2021-01-16 11:37:12 +1000

    Chore: Check content module running in background

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -3,3 +3,13 @@ import '../../assets/img/icon-128.png';
 
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+let count = 0;
+
+let timer = setInterval(() => {
+  count++;
+  console.log('counting ' + count);
+
+  if (count >= 10) {
+    clearInterval(timer);
+  }
+}, 1000);
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,8 @@
 import { printLine } from './modules/print';
 
 console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+console.warn('Must reload extension for modifications to take effect.');
+
+console.log('location: ' + window.location);
 
 printLine("Using the 'printLine' function from the Print Module");
